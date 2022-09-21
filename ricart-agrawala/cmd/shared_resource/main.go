28cb94d9c260d2fc14f19235bb60749bd7743cc2
atedesch1 <alvarotedeschi@gmail.com>
2022-09-21 01:05:13 -0300

    implement simple shared resource

--- ricart-agrawala/cmd/shared_resource/main.go
+++ ricart-agrawala/cmd/shared_resource/main.go
@@ -1,13 +1,27 @@
 package main
 
-import "net"
+import (
+	"fmt"
+	"net"
+)
+
+var protocol string = "udp"
+var address string = "127.0.0.1:10000"
 
 func main() {
-	address, _ := net.ResolveUDPAddr("udp", ":10001")
-	connection, _ := net.ListenUDP("udp", address)
+	addr, _ := net.ResolveUDPAddr(protocol, address)
+	connection, _ := net.ListenUDP(protocol, addr)
 
 	defer connection.Close()
 
+	buf := make([]byte, 1024)
+
 	for {
+		n, addr, err := connection.ReadFromUDP(buf)
+		fmt.Println("Received ", string(buf[0:n]), " from ", addr)
+
+		if err != nil {
+			fmt.Println("Error: ", err)
+		}
 	}
 }
